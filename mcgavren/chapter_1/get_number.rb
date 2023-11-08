b00e3c473b2b5d25cf3b8b07f3f5540499454289
akmatkulov <pnz19620@gmail.com>
2023-11-09 05:40:23 +0600

    Fixed get number

--- mcgavren/chapter_1/get_number.rb
+++ mcgavren/chapter_1/get_number.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-puts "Welcome to 'Get My Number"
-print "What's your name?: "
-name = gets.chomp.capitalize
-puts "Welcome #{name}!"
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+name = gets.chomp
+puts "Welcome, #{name}!"
 
 puts "I've got a random number between 1 and 100."
 puts 'Can you guess it?'
 target = rand(1..100)
-guesses_count = 0
-check = true
-while guesses_count < 10 && check
-  puts "You've got #{10 - guesses_count} guesses left."
-  print 'Enter number: '
+
+num_guesses = 0
+check = false
+
+until num_guesses == 10 || check
+  puts "You've got #{10 - num_guesses} guesses left."
+  print 'Make a guess: '
   guess = gets.to_i
-  guesses_count += 1
-  case target <=> guess
-  when -1
-    puts 'Oops. Your guess was HIGH'
-  when 1
-    puts 'Oops. Your guess was LOW'
-  when 0
-    puts "Good job, #{name}!"
-    puts "You guessed my number in #{guesses_count} guesses!"
-    check = false
+  num_guesses += 1
+  puts 'Oops. Your guess was LOW.' if guess < target
+  puts 'Oops. Your guess was HIGH.' if guess > target
+  if guess == target
+    puts "You win! #{target}"
+    check = true
   end
 end
 
-puts "Sorry. You didn't get my number. (It was #{target}.)" if check
+puts "You loose sorry! #{target}" unless check
